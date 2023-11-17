88828576fdcb6cd7637ece06f8fea1afa95f2cd1
Jhenifer Ribeiro <jheniferribeirolopes_@hotmail.com>
2023-11-17 16:44:20 -0300

    Projeto em construção

---
+++ script.js
@@ -0,0 +1,14 @@
+const wrapper = document.querySelector(".wrapper"),
+    musicImg = wrapper.querySelector(".img-area img"),
+    musicName = wrapper.querySelector(".song-details .name"),
+    musicArtist = wrapper.querySelector(".song-details .artist"),
+    playPauseBtn = wrapper.querySelector(".play-pause"),
+    prevBtn = wrapper.querySelector("#prev"),
+    nextBtn = wrapper.querySelector("#next"),
+    mainAudio = wrapper.querySelector("#main-audio"),
+    progressBar = wrapper.querySelector(".progress-bar"),
+    progressArea = wrapper.querySelector(".progress-area"),
+    songList = wrapper.querySelector(".song-list"),
+    moreMusicBtn = wrapper.querySelector("#more-music"),
+    closemoreMusic = wrapper.querySelector("#clase")
+    
---
+++ song-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+    {
+        name: "Space Song",
+        artist: "Beach House",
+        img: "img-1",
+        src: "musica-1"
+    },
+    {
+        name: "Myth",
+        artist: "Beach House",
+        img: "img-2",
+        src: "musica-2"
+    },
+    {
+        name: "Call It Fate, Call It Karma",
+        artist: "The Strokes",
+        img: "img-3",
+        src: "musica-3"
+    }
+]
